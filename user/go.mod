d558fc50c050701941a6fa02fcb17d61fac6e0bd
azhuang <3499296286@qq.com>
2021-01-06 23:03:47 +0800

    6.handler编写，main.go初始化等操作

--- user/go.mod
+++ user/go.mod
@@ -3,7 +3,11 @@ module git.imooc.com/cap1573/user
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.4.3
 	github.com/jinzhu/gorm v1.9.16
-	github.com/micro/go-micro/v2 v2.9.1 // indirect
-	github.com/micro/micro/v3 v3.0.4
+	github.com/micro/go-micro/v2 v2.9.1
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	google.golang.org/protobuf v1.25.0
 )
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
--- user/main.go
+++ user/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"git.imooc.com/cap1573/user/domain/repository"
+	service2 "git.imooc.com/cap1573/user/domain/service"
 	"git.imooc.com/cap1573/user/handler"
-	pb "git.imooc.com/cap1573/user/proto"
-
-	"github.com/micro/micro/v3/service"
-	"github.com/micro/micro/v3/service/logger"
+	user "git.imooc.com/cap1573/user/proto/user"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/micro/go-micro/v2"
 )
 
 func main() {
-	// Create service
-	srv := service.New(
-		service.Name("user"),
-		service.Version("latest"),
+	//服务参数设置
+	srv := micro.NewService(
+		micro.Name("go.micro.service.user"),
+		micro.Version("latest"),
 	)
+	//初始化服务
+	srv.Init()
 
-	// Register handler
-	pb.RegisterUserHandler(srv.Server(), new(handler.User))
-
-	// Run service
-	if err := srv.Run(); err != nil {
-		logger.Fatal(err)
+	//创建数据库连接
+	db, err := gorm.Open("mysql", "root:root@/micro?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer db.Close()
+	db.SingularTable(true)
+
+	//只执行一次
+	rp := repository.NewUserRepository(db)
+	rp.InitTable()
+
+	//创建服务实例
+	userDataService := service2.NewUserDataService(repository.NewUserRepository(db))
+
+	err = user.RegisterUserHandler(srv.Server(), &handler.User{UserDataService: userDataService})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+
 }
